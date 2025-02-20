1921fd824516905b10c21382899cf3ff3650a4f1
himansh-01 <himanshprajapati2004@gmail.com>
2025-02-20 12:24:01 +0530

    hamburger navbar

--- front/src/components/parts/navbar.tsx
+++ front/src/components/parts/navbar.tsx
@@ -1,14 +1,18 @@
-
+'use client'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from '../ui/button'
 import Image from 'next/image'
 import Logo from '../../../public/gslogo.svg';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoCloseSharp } from "react-icons/io5";
 // import { cookies } from 'next/headers';
 // import { redirect } from 'next/navigation';
 const Navbar =  () => {
 
+    const [click, setClick] = useState(false)
 
+    const handleClick = () => {setClick(!click)}
 
     // commented this only for the purpose of testing the logout function
 
@@ -28,9 +32,24 @@ const Navbar =  () => {
         <>
             <nav style={{ zIndex: "100" }} className="container bg-darkblue customnav min-w-full  px-10  sticky top-0  py-4">
                 <div className="flex items-center  justify-between">
-                    <Link href="/" className="flex items-center space-x-2">
+                    <Link href="/" className="md:flex hidden items-center space-x-2">
                         <Image src={Logo} alt="logo" />
                     </Link>
+                    <GiHamburgerMenu className='md:hidden block text-[#015aff] h-8 w-8' onClick={handleClick} />
+                        <div className='w-[100vw] h-[100vh] left-0 top-0 absolute flex z-40' style={{
+                             opacity : click ? 1 : 0 ,
+                             visibility : click ? "visible" : "hidden",
+                        }}>
+                            <div className='w-[60%] h-[100vh]  flex-[0.6] bg-[#015aff] px-12 py-8'>
+                                <div className='w-[95%]'><IoCloseSharp className='h-8 w-8 grid justify-self-end' onClick={handleClick} /></div>
+                                <div className='flex flex-col gap-6 px-10 py-4'>
+                                    <Link href="/home" className="hover:text-white text-2xl">Home</Link>
+                                    <Link href="/about" className="hover:text-white text-2xl">About</Link>
+                                    <Link href="https://blog.growthsq.in/" className="hover:text-white text-2xl">Blog</Link>
+                                </div>
+                            </div>
+                            <div className='w-[40%] h-[100vh] bg-black opacity-30 flex-[0.4]' onClick={handleClick}></div>
+                        </div>
                     <div className="hidden md:flex items-center space-x-6 text-gray-300">
                         <Link href="/home" className="hover:text-white">Home</Link>
                         <div className='h-full w-1 '>|</div>
