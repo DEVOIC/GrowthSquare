d8ba07dea5ff272427daed373bcdfad26b6ca516
Nitin Chakrawarti <149225785+NitinChakrawarti@users.noreply.github.com>
2025-04-20 00:26:44 +0530

    Fix: Added new founding member data for about page

--- front/src/data/foundingmemebers.ts
+++ front/src/data/foundingmemebers.ts
@@ -18,6 +18,15 @@ const foundingMemebers: Member[] = [
       instagram: "https://instagram.com/username"
     },
     {
+      image:"https://drive.google.com/uc?id=1cK9tKKdh1q6GwIvuAZr2_O8aEKMD0Zx-",
+      name:"Madhvendra Pratap Singh",
+      role:"Project Manager",
+      linkedin: "https://www.linkedin.com/in/madhvendra-connects/",
+      github: "https://github.com/username",
+      twitter: "https://x.com/madhav_0399",
+      instagram: "https://instagram.com/username"
+    },
+    {
         image: "https://drive.google.com/uc?id=1wCwx6PxQD1KT2OcpqAzsfSZHXg0edQHH",
         name: "Utkarsh Gupta",
         role: "Human Resource Manager",
