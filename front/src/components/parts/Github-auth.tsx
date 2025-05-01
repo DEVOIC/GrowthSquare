8e7381bdcd03840eb7afc2a860ecc78f049715b1
himansh-01 <himanshprajapati2004@gmail.com>
2025-05-01 21:01:20 +0530

    google-auth api merge

--- front/src/components/parts/Github-auth.tsx
+++ front/src/components/parts/Github-auth.tsx
@@ -3,24 +3,29 @@ import { app } from "@/lib/firebase";
 import { getAuth, GithubAuthProvider, signInWithPopup } from "firebase/auth";
 import { Button } from "../ui/button";
 import { FaGithub } from "react-icons/fa";
+import axios from "axios";
 
 
 const githubAuth = () => {
     const handleGithub = async () => {
     const auth = getAuth(app)
     const provider = new GithubAuthProvider()
-     try{
+    try{
         const results = await signInWithPopup(auth, provider)
         console.log(results)
-        try{
-            //api call
-        }
-        catch(error){
-            console.log(error)
-        }
+        const token = await results.user.getIdToken()
+
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_BACK_API}/${process.env.NEXT_PUBLIC_ROUTE}/auth/google-login`,{
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token
+            }
+     })
+
+     console.log(response.data)
      }
      catch(error){
-        console.log(error)
+        console.error("Error during sign-in:",error)
     }
   }
    
--- front/src/components/parts/Google-auth.tsx
+++ front/src/components/parts/Google-auth.tsx
@@ -3,6 +3,7 @@ import { app } from "@/lib/firebase";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { Button } from "../ui/button";
 import { FcGoogle } from "react-icons/fc";
+import axios from "axios";
 
 
 const googleAuth = () => {
@@ -12,15 +13,19 @@ const googleAuth = () => {
      try{
         const results = await signInWithPopup(auth, provider)
         console.log(results)
-        try{
-            //api call
-        }
-        catch(error){
-            console.log(error)
-        }
+        const token = await results.user.getIdToken()
+
+        const response = await axios.post(`${process.env.NEXT_PUBLIC_BACK_API}/${process.env.NEXT_PUBLIC_ROUTE}/auth/google-login`,{
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token
+            }
+     })
+
+     console.log(response.data)
      }
      catch(error){
-        console.log(error)
+        console.error("Error during sign-in:",error)
     }
   }
    
